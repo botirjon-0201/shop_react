c904b914cfb3531015d0f14ad6811f2a19ef0bb0
Botirjon <botirjonumurzoqov02.01.1991@gmail.com>
2022-12-17 17:35:15 +0500

    some changes

--- src/components/BasketList.js
+++ src/components/BasketList.js
@@ -10,20 +10,22 @@ function BasketList() {
   }, 0);
 
   return (
-    <ul className="collection basket-list">
-      <li className="collection-item active">Basket</li>
-      {order.length ? (
-        order.map((item) => {
-          return <BasketItem key={item.mainId} {...item} />;
-        })
-      ) : (
-        <li className="collection-item">Basket is empty</li>
-      )}
-      <li className="collection-item active">Total cost: {totalPrice}</li>
-      <i className="material-icons basket-close" onClick={handleBasketShow}>
-        close
-      </i>
-    </ul>
+    <div className="bsk">
+      <ul className="collection basket-list">
+        <li className="collection-item active">Basket</li>
+        {order.length ? (
+          order.map((item) => {
+            return <BasketItem key={item.mainId} {...item} />;
+          })
+        ) : (
+          <li className="collection-item">Basket is empty</li>
+        )}
+        <li className="collection-item active">Total cost: {totalPrice}</li>
+        <i className="material-icons basket-close" onClick={handleBasketShow}>
+          close
+        </i>
+      </ul>
+    </div>
   );
 }
 
